758d9a9889ecff606f43307338ac00eeb6def292
Benjamín de la Fuente Ranea <yosoymin@gmail.com>
2018-07-09 18:20:29 +0200

    WIP: first fulfillment

--- google-assistant-sdk/googlesamples/assistant/grpc/actions/webhooks/index.js
+++ google-assistant-sdk/googlesamples/assistant/grpc/actions/webhooks/index.js
@@ -3,9 +3,11 @@ const {
     actionssdk,
     Image,
 } = require('actions-on-google')
+var express = require('express')
+const bodyParser = require('body-parser')
 
 // Create an app instance
-const app = actionssdk()
+const app = actionssdk();
 
 // Register handlers for Actions SDK intents
 app.intent('actions.intent.MAIN', conv => {
@@ -14,29 +16,13 @@ app.intent('actions.intent.MAIN', conv => {
         '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>');
 })
 
-app.intent('actions.intent.TEXT', (conv, input) => {
+app.intent('PlayInfo', (conv, input) => {
     if (input === 'bye') {
         return conv.close('Goodbye!');
     }
-    conv.ask('<speak>You said, ' +
-        `<say-as interpret-as="ordinal">${input}</say-as></speak>`);
+    //conv.ask('<speak>You said, ' + `<say-as interpret-as="ordinal">${input}</say-as></speak>`);
+    conv.ask('Está sonando algo increible para mi');
 })
-
-/*const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
 
 var https = require('https');
 var http = require('http');
@@ -48,13 +34,16 @@ var options = {
     cert: fs.readFileSync('./client-cert.pem')
 };
 
+var expressApp = express();
+expressApp.use(bodyParser.json(), app);
+
 // Create an HTTP service.
-//http.createServer(app).listen(80);
-http.createServer((req, res) => {
+http.createServer(expressApp).listen(80);
+/*http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
-}).listen(80);
+}).listen(80);*/
 
 // Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(443);
+https.createServer(options, expressApp).listen(443);
